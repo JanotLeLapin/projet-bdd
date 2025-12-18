87e73b4f17f6e1f9f70fb9c09527a953cc83d9f4
Joseph DALY <joseph300905@gmail.com>
2025-12-18 20:57:03 +0100

    load: stores

---
+++ load/stores.sql
@@ -0,0 +1,14 @@
+OPTIONS (SKIP=1)
+LOAD DATA
+INFILE 'stores.csv'
+APPEND
+INTO TABLE store_
+FIELDS TERMINATED BY ','
+TRAILING NULLCOLS
+(
+    email,
+    pays,
+    date_ouverture DATE "YYYY-MM-DD HH24:MI:SS",
+    nom,
+    id_gestionnaire
+)
